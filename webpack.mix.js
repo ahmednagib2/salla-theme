3390edd937ad5db6b2187cd54d1bbd3dc2418c45
ahmednagib2 <ahmednagib300@gmail.com>
2025-04-04 04:02:39 +0300

    تحديث ملفات الأنماط وإعداد webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,9 @@
 const mix = require('laravel-mix');
 
 mix.js('src/assets/js/app.js', 'dist/js')
-   .sass('src/assets/styles/app.scss', 'dist/css')
-   .options({
-       processCssUrls: false,
-       postCss: [
-           require('tailwindcss'),
-           require('autoprefixer'),
-       ],
-   })
+   .postCss('src/input.css', 'dist/css', [
+       require('tailwindcss'),
+       require('autoprefixer'),
+   ])
    .copyDirectory('src/assets/images', 'dist/images')
    .version();
